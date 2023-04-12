21f62016d475deee1ceabdf0813888ac6ca3071b
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-04-13 00:41:44 +0900

    Feat : spring 설정

---
+++ src/main/java/com/twoLeader/twoLeader/TwoLeaderApplication.java
@@ -0,0 +1,13 @@
+package com.twoLeader.twoLeader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoLeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoLeaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twoLeader/twoLeader/TwoLeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twoLeader.twoLeader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoLeaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
