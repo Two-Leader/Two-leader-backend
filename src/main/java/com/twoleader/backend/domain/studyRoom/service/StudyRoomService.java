cb2476a794dcc317498465b1f60c785677fce4de
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-04-22 04:51:26 +0900

    Fix:StudyRoom Service Mapper없어진거 수정

--- src/main/java/com/twoleader/backend/domain/studyRoom/service/StudyRoomService.java
+++ src/main/java/com/twoleader/backend/domain/studyRoom/service/StudyRoomService.java
@@ -3,18 +3,17 @@ package com.twoleader.backend.domain.studyRoom.service;
 import com.twoleader.backend.domain.studyRoom.dto.request.CreateStudyRoomDto;
 import com.twoleader.backend.domain.studyRoom.dto.response.FindStudyRoomDto;
 import com.twoleader.backend.domain.studyRoom.entity.StudyRoom;
-import com.twoleader.backend.domain.studyRoom.mapper.StudyRoomMapper;
 import com.twoleader.backend.domain.studyRoom.repository.StudyRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class StudyRoomService {
     private final StudyRoomRepository studyRoomRepository;
-    private final StudyRoomMapper studyRoomMapper;
 
     public StudyRoom createStudyRoom(CreateStudyRoomDto createStudyRoomDto) {
         StudyRoom studyRoom = StudyRoom.builder()
@@ -25,6 +24,6 @@ public class StudyRoomService {
 
     public List<FindStudyRoomDto> findAllStudyRoom(){
         List<StudyRoom> rooms = studyRoomRepository.findAll();
-        return studyRoomMapper.toDto(rooms);
+        return rooms.stream().map(StudyRoom::toDto).collect(Collectors.toList());
     }
 }
