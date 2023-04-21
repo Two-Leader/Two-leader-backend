f15987e58ff148f295dc631e3fa26e53f318b12c
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-04-22 01:17:03 +0900

    Fix:Swagger 수정

--- src/main/java/com/twoleader/backend/global/config/OpenApiConfig.java
+++ src/main/java/com/twoleader/backend/global/config/OpenApiConfig.java
@@ -1,21 +1,30 @@
 package com.twoleader.backend.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /** Swagger springdoc-ui 구성 파일 */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TwoLeader API Document",
+                description = "TwoLeader 프로젝트의 API 명세서",
+                version = "v1"
+        )
+)
 @Configuration
 public class OpenApiConfig {
   @Bean
-  public OpenAPI openAPI() {
-    Info info =
-        new Info()
-            .title("TwoLeader API Document")
-            .version("v0.0.1")
-            .description("TwoLeader 프로젝트의 API 명세서입니다.");
-    return new OpenAPI().components(new Components()).info(info);
+  public GroupedOpenApi StudyApi(){
+    String[] ptahs = {"/api/v1/**"};
+
+    return GroupedOpenApi.builder()
+            .group("Study API v1")
+            .pathsToMatch(ptahs)
+            .build();
   }
 }
