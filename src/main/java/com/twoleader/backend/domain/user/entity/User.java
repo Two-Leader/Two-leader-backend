9ebd051e22055be8eab1be819fd756d1c9669fb0
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-04-23 19:04:40 +0900

    Fix: User Entity user_name 추가 및 toDto 추가

--- src/main/java/com/twoleader/backend/domain/user/entity/User.java
+++ src/main/java/com/twoleader/backend/domain/user/entity/User.java
@@ -2,18 +2,37 @@ package com.twoleader.backend.domain.user.entity;
 
 
 import com.twoleader.backend.domain.studyRoom.entity.StudyRoom;
+import com.twoleader.backend.domain.user.dto.response.GetUserResponse;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
+@ToString(callSuper = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id;
 
-    private String user_uuid;
+    @Column(nullable = false)
+    @Builder.Default
+    private UUID user_uuid = UUID.randomUUID();
+
+    @Column(nullable = false)
+    private String user_name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
     private StudyRoom room;
+
+    public GetUserResponse toDto(){
+        return GetUserResponse.builder()
+                .user_uuid(this.user_uuid)
+                .user_name(this.user_name)
+                .build();
+    }
 }
