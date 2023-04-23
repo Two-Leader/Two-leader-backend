06bbe787c5edebda1dbcb943cfb360f3de020c32
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-04-23 19:08:08 +0900

    Fix: StudyRoom user컬럼 임시 삭제

--- src/main/java/com/twoleader/backend/domain/studyRoom/entity/StudyRoom.java
+++ src/main/java/com/twoleader/backend/domain/studyRoom/entity/StudyRoom.java
@@ -1,34 +1,36 @@
 package com.twoleader.backend.domain.studyRoom.entity;
 
-import com.twoleader.backend.domain.studyRoom.dto.response.FindStudyRoomDto;
+import com.twoleader.backend.domain.studyRoom.dto.response.GetStudyRoomResponse;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Entity
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
 @ToString(callSuper = true)
 public class StudyRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long room_id;
 
+    @Column(nullable = false)
     @Builder.Default
     private UUID room_uuid = UUID.randomUUID();
 
     @Column(nullable = false)
     private String room_name;
 
-    public static FindStudyRoomDto toDto(StudyRoom studyRoom){
-        return FindStudyRoomDto.builder()
-                .room_uuid(studyRoom.getRoom_uuid())
-                .room_name(studyRoom.getRoom_name())
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<User> users = new ArrayList<>();
+
+    public GetStudyRoomResponse toDto(){
+        return GetStudyRoomResponse.builder()
+                .room_uuid(this.room_uuid)
+                .room_name(this.room_name)
                 .build();
     }
 }
