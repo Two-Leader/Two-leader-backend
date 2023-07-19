dc0563f8ee734497619de039f912b920492f0d43
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-07-19 19:06:13 +0900

    feat: kafka config

--- src/main/java/com/twoleader/backend/global/config/kafka/KafkaProducerConfig.java
+++ src/main/java/com/twoleader/backend/global/config/kafka/KafkaProducerConfig.java
@@ -15,19 +15,20 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @EnableKafka
 @Configuration
-public class ProducerConfig {
+public class KafkaProducerConfig {
 
   @Value(value = "${spring.kafka.producer.bootstrap-servers}")
   private String bootstrapServer;
 
-  // Kafka ProducerFactory를 생성하는 Bean 메서드
   @Bean
+  public KafkaTemplate<String, ChatMessage> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
+  }
+
   public ProducerFactory<String, ChatMessage> producerFactory() {
     return new DefaultKafkaProducerFactory<>(producerConfigurations());
   }
 
-  // Kafka Producer 구성을 위한 설정값들을 포함한 맵을 반환하는 메서드
-  @Bean
   public Map<String, Object> producerConfigurations() {
     HashMap<String, Object> configurations = new HashMap<>();
     configurations.put(
@@ -39,11 +40,5 @@ public class ProducerConfig {
         org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
         JsonSerializer.class);
     return configurations;
-  }
-
-  // KafkaTemplate을 생성하는 Bean 메서드
-  @Bean
-  public KafkaTemplate<String, ChatMessage> kafkaTemplate() {
-    return new KafkaTemplate<>(producerFactory());
   }
 }
